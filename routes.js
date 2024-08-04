2cdce3043e54b4a0d296b812eaea57c9d88d11c0
sterriharder <sterriharder@gmail.com>
2024-08-04 17:20:14 +0900

    system message improve and logging

--- routes.js
+++ routes.js
@@ -28,11 +28,12 @@ router.post('/chat', async (req, res) => {
         if(service_gbn_header == "aq"){
             systemMessage  += "여러가지 묻는 질문에 쉽게 알아들을수 있도록 자세히 알려줘. ";
         } else if (service_gbn_header == "hh"){
-            systemMessage  += "병원이나 진료과목, 몸상태나 증상, 건강 등과 관련된 질문에는 제대로 답해주고, 관련된 병원도 알려줘(예: 내과, 안과 등). "
-                            + "그리고 병원정보나 진료과목, 몸상태나 증상, 건강 등과 관련 없는 질문에는 이렇게 답해줘. "
+            systemMessage  += "증상이나 몸상태, 건강, 병원 관련 질문에는 이런식으로 알려줘. "
+                            + "당신의 증상이 어떤 상태나 질병인것 같다. 그럴때는 어느 병원(진료과목)에 가는것이 좋겠다. 근처의 병원을 추천해주면 더 좋아. "
+                            + "그리고 증상이나 몸상태, 건강, 병원과 관련 없는 질문에는 이렇게 답해줘. "
                             + "'몸상태나 증상, 병원과 관련된 질문만 부탁드려요😁' ";
         }
-
+console.log(systemMessage);
         const userMessage = req.body.message;
 
         const response = await axios.post('https://api.openai.com/v1/chat/completions', {
